6b6be35219248a02d846aa4dd47be9229028ec9c
Angel Vanegas <angelman@gmail.com>
2020-03-29 00:44:14 -0400

    add logging

--- app/workers/csv_processor.rb
+++ app/workers/csv_processor.rb
@@ -1,8 +1,16 @@
 class CsvProcessor
     include Sidekiq::Worker
 
-    def perform(args)
-        p "*" * 30
-        p "IT'S DONE"
+    require 'csv'
+    def perform(identifier)
+        csv = CsvFile.find_by_identifier(identifier)
+        Rails.logger.debug("*" * 30)
+        Rails.logger.debug "IT'S DONE"
+    end
+
+    def csv_process
+        # CSV.parse(csv.file.download, headers: true) do |row|
+        #     processing
+        # end
     end
 end
