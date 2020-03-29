c90408cd1084fc07c7aff4d632e196b7fd659faf
Angel Vanegas <angelman@gmail.com>
2020-03-29 16:32:05 -0400

    remove background job for testing

--- app/jobs/csv_processor.rb
+++ app/jobs/csv_processor.rb
@@ -1,5 +1,5 @@
-class CsvProcessor < ApplicationJob
-    queue_as :low_priority
+class CsvProcessor #< ApplicationJob
+    # queue_as :low_priority
 
     require 'csv'
     def perform(identifier)
@@ -9,11 +9,21 @@ class CsvProcessor < ApplicationJob
 
     private 
     def csv_process(csv, identifier)
+        rows = ProcessedCsv.where(identifier: identifier)
+        csv_file = csv.file.download
         CSV.parse(csv_file, headers: true).each_with_index do |row, i|
             row_hash = csv_hash(row)
             temp = ProcessedCsv.find_by_row_number(i)
             if temp.present?
-                temp.update_columns(row_hash)
+                if (csv_file.split.size - 1) != (rows.size)
+                    ProcessedCsv.delete(rows)
+                    hashed = row_hash
+                    row_hash[:identifier] = identifier
+                    row_hash[:row_number] = i
+                    temp = ProcessedCsv.new(row_hash)
+                else
+                    temp.update_columns(row_hash)
+                end
             else
                 hashed = row_hash
                 row_hash[:identifier] = identifier
